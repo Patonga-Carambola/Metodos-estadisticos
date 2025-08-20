69ab50c503eeb96b26e7a357c29edce184691502
Oscar <chimultuku@gmail.com>
2025-08-20 08:25:57 -0600

    New

---
+++ Laboratorio 3.R
@@ -0,0 +1,20 @@
+# Laboratorio Semana 3
+# 20/08/2025
+# Oscar Alejandro mtz. y.
+
+
+# Importar datos --------------------------------------------------------
+
+
+
+# Graficar datos --------------------------------------------------------
+
+Temp <- read.csv("temperatura.csv", header = T)
+Temp <- read.csv("Data/Medias_temp.csv", header = T)
+# Estadísticas descriptivas -----------------------------------------------
+
+
+
+
+
+
