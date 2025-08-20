5a4d45f37898e5129e2130cb74c902652b8e8185
Oscar <chimultuku@gmail.com>
2025-08-20 09:06:52 -0600

    Update

--- Laboratorio 3.R
+++ Laboratorio 3.R
@@ -5,14 +5,39 @@
 
 # Importar datos --------------------------------------------------------
 
-
-
-# Graficar datos --------------------------------------------------------
-
 Temp <- read.csv("temperatura.csv", header = T)
 Temp <- read.csv("Data/Medias_temp.csv", header = T)
+
+
+# Ingresar datos de manera manual --------------------------------------------------------
+
+edad <- c(18, 19, 18, 18, 25, 19, 18, 18, 18, 17, 19, 19, 18, 17, 19, 
+            18, 19, 19)
+alumno <-  seq(1,18,1)
+
+info <- data.frame(alumno, edad)
+
+info$altura <- c(174, 174, 170, 160, 158, 155, 188, 170, 175, 170, 172, 170, 
+                174, 180, 158, 161, 188, 164)
+# Graficar datos ----------------------------------------------------------
+
+boxplot(info$altura, 
+        col="peru",
+        main = "clase 3 semestre")
+colores = c("peru", "palegreen", "powderblue")
+boxplot(datos_meses, 
+        col = colores,
+        main = "Temperaturas")
+
+
+
+
 # Estadísticas descriptivas -----------------------------------------------
 
+
+
+
+# Estadistica inferencial -------------------------------------------------
 
 
 
