ae8e8a765367bdbcb4cd871f3a529196605d795a
Oscar <chimultuku@gmail.com>
2025-09-03 09:22:19 -0600

    Update

---
+++ MH02.R
@@ -0,0 +1,49 @@
+# Base de datos Iris ------------------------------------------------------
+
+data_sub <- subset(iris, Species %in% c("versicolor", "virginica", "setosa"))
+iris <- subset(iris, Species %in% c("versicolor", "virginica", "setosa"))
+head(data_sub)
+summary(data_sub)
+
+#  Prueba estadística -----------------------------------------------------
+
+#Medidas de tendencia central de Petal.Length
+tapply(data_sub$Petal.Length, data_sub$Species, summary)
+
+#¿Existe una diferencia significativa en la longitud del pétalo entre Iris versicolor e Iris virginica?
+#hipotesis nula: no existe una diferencia
+#hipotesis alternativa: si existe diferencia
+
+t.test(
+  iris$Petal.Length[iris$Species == "versicolor"],
+  iris$Petal.Length[iris$Species == "virginica"],
+  alternative = "two.sided")
+
+#calcular la diferencia con Cohen's d. Instala el paquete effsize
+library(effsize)
+
+cohen.d(
+  iris$Petal.Length[iris$Species == "versicolor"],
+  iris$Petal.Length[iris$Species == "virginica"])
+
+#d ≈ 0.2: tamaño de efecto pequeño
+#d ≈ 0.5: tamaño de efecto mediano
+#d ≥ 0.8: tamaño de efecto grande
+#resultado =-2.520756 (largo), es un cambio muy grande entre las dos especies
+
+
+
+#  Visualización ----------------------------------------------------------
+
+# Filtrar solo las especies que te interesan
+iris_sub <- subset(iris, Species %in% c("versicolor", "virginica"))
+
+# Generar el boxplot
+boxplot(Petal.Length ~ Species, data=iris_sub,
+        main = "Comparación de Petal.Length entre Versicolor y Virginica",
+        xlab = "Especie",
+        ylab = "Petal.Length",
+        col = c("lightblue", "lightgreen"))
+
+
+
