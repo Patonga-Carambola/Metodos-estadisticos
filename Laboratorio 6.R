907894fb400e583fe97cf5544777175fb6fdbcfb
Oscar <chimultuku@gmail.com>
2025-09-25 09:25:56 -0600

    update

--- Laboratorio 6.R
+++ Laboratorio 6.R
@@ -29,11 +29,41 @@ cor.test(faithful$waiting, faithful$eruptions,
 # ========================================================
 cor.test(faithful$waiting, faithful$eruptions,
          method = "spearman")
+# ========================================================
+# Tiempo de reacción
+# ========================================================
+resp
+resp <- data.frame(Tiempo = c(12, 15, 17, 18, 20, 21, 22, 26),
+                   Edad = c(13, 25, 20, 35, 45, 30, 60, 95))
+resp$Rango_Tiempo <- rank(resp$Tiempo, ties.method = "first")
+resp$Rango_Edad <- rank(resp$Edad, ties.method = "first")
+plot(resp$Tiempo, resp$Edad)
 
+resp$dif <- resp$Rango_Tiempo - resp$Rango_Edad
+resp$dif2 <- resp$dif^2
+sum(resp$dif2)
+cor.test(resp$Rango_Tiempo, resp$Rango_Edad, method = "spearman")
 
+tau <- data.frame(A = c(1, 2, 3, 4, 5, 6), B = c(3, 1, 4, 2, 6, 5))
+cor.test(tau$A, tau$B, method = "kendall")
 
+set.seed(123) # para reproducibilidad
 
+# Numero de observaciones
+set.seed(123)
+n <- 20
 
+Horas_estudio <- sample(1:10, n, replace = TRUE)
 
+Resultados <- sapply(Horas_estudio, function(horas) {ifelse(runif(1) < (horas / 10), "Aprobado", "Reprobado")})
 
+estudio <- data.frame( estudiante = 1:n, Horas_estudio, Resultados)
+estudio$Resultado_bin <- ifelse(estudio$Resultados == "Aprobado", 1, 0)
+head(estudio)
+
+cor.test(estudio$Horas_estudio, estudio$Resultado_bin, method = "pearson")
+
+mean_aprobados <- mean(estudio$Horas_estudio[estudio$Resultados == "Aprobado"])
+
+mean_reprobado <- mean(estudio$Horas_estudio[estudio$Resultados == "Reprobados"])
 
